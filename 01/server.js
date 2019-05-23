96603f63cb0f8fa9329dc3648cbcfb07128de734
pdvonzoo <a01088570194@gmail.com>
2019-05-23 19:57:58 +0900

    Feat: restful api 추가

--- 01/server.js
+++ 01/server.js
@@ -18,18 +18,11 @@ app.get("/api/courses", (req, res) => {
 });
 
 app.post("/api/course", (req, res) => {
-  const schema = {
-    name: Joi.string()
-      .min(3)
-      .required()
-  };
-
-  const result = Joi.validate(req.body, schema);
-
-  if (req.body.name || req.body.name.length < 3) {
-    res.status(400).send(result.error.details[0].message);
-    return;
+  const { error } = validateCourse(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
   }
+
   const course = {
     id: courses.length + 1,
     name: req.body.name
@@ -38,10 +31,43 @@ app.post("/api/course", (req, res) => {
   res.send(course);
 });
 
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("The course with the given ID was not found");
+
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("The course with the given ID was not found");
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+
+  return Joi.validate(course, schema);
+}
+
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
   if (!course)
-    res.status(404).send("The course with the given ID was not found");
+    return res.status(404).send("The course with the given ID was not found");
   res.send(course);
 });
 
